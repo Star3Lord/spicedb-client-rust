9314d0ca8714b5c002aa5566b2e8a1936cdf16d5
Nikhil Patel <nikhil3patel@gmail.com>
2024-05-02 15:19:58 -0400

    updated package to align with structure of authzed libraries

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,19 @@ mod proto {
     tonic::include_proto!("mod");
 }
 
-#[cfg(feature = "v0")]
-pub mod v0 {
-    pub use super::proto::authzed::api::v0::*;
-}
+pub mod api {
+    #[cfg(feature = "v0")]
+    pub mod v0 {
+        pub use super::super::proto::authzed::api::v0::*;
+    }
 
-#[cfg(feature = "v1")]
-pub mod v1 {
-    pub use super::proto::authzed::api::v1::*;
-}
+    #[cfg(feature = "v1")]
+    pub mod v1 {
+        pub use super::super::proto::authzed::api::v1::*;
+    }
 
-#[cfg(feature = "v1alpha1")]
-pub mod v1alpha1 {
-    pub use super::proto::authzed::api::v1alpha1::*;
+    #[cfg(feature = "v1alpha1")]
+    pub mod v1alpha1 {
+        pub use super::super::proto::authzed::api::v1alpha1::*;
+    }
 }
