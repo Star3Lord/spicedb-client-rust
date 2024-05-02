f5b601fdd2a57c91ead2b268b8c60a8ce8df84cd
Nikhil Patel <nikhil3patel@gmail.com>
2024-05-02 15:00:45 -0400

    updated build script

--- build.rs
+++ build.rs
@@ -1,13 +1,24 @@
 fn main() {
+    let proto_files: Vec<std::path::PathBuf> = glob::glob("proto/**/*.proto")
+        .unwrap()
+        .into_iter()
+        .filter_map(Result::ok)
+        .collect();
+
+    // Tell cargo to recompile if any of these proto files are changed
+    for proto_file in &proto_files {
+        println!("cargo:rerun-if-changed={}", proto_file.display());
+    }
+
+    let descriptor_path =
+        std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("proto_descriptor.bin");
+
     tonic_build::configure()
-        .build_server(false)
-        .compile(
-            &[
-                "proto/authzed/api/v1/permission_service.proto",
-                "proto/authzed/api/v1/schema_service.proto",
-                "proto/authzed/api/v1/watch_service.proto",
-            ],
-            &["proto"],
-        )
+        .server_mod_attribute("attrs", "#[cfg(feature = \"server\")]")
+        .client_mod_attribute("attrs", "#[cfg(feature = \"client\")]")
+        .file_descriptor_set_path(&descriptor_path)
+        .include_file("mod.rs")
+        // .out_dir("src/proto_build")
+        .compile(&proto_files, &["proto"])
         .unwrap();
 }
